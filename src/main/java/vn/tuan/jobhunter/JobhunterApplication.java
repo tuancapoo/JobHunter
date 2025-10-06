d0d3b0d1b54c6de4595c203521ce38259f645b04
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2025-10-06 12:52:33 +0700

    ok

--- src/main/java/vn/tuan/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/tuan/jobhunter/JobhunterApplication.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter;
+package vn.tuan.jobhunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @SpringBootApplication
 public class JobhunterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobhunterApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(JobhunterApplication.class, args);
 	}
 
 }
--- src/test/java/vn/tuan/jobhunter/JobhunterApplicationTests.java
+++ src/test/java/vn/tuan/jobhunter/JobhunterApplicationTests.java
@@ -1,4 +1,4 @@
-package vn.hoidanit.jobhunter;
+package vn.tuan.jobhunter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
