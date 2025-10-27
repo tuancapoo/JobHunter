ab46e66eeb33134596e63316400e111aaa648b8b
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2025-10-28 00:17:24 +0700

    CronJobTest

--- src/main/java/vn/tuan/jobhunter/JobhunterApplication.java
+++ src/main/java/vn/tuan/jobhunter/JobhunterApplication.java
@@ -2,6 +2,8 @@ package vn.tuan.jobhunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //disable security
 //@SpringBootApplication(exclude = {
@@ -11,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //enable
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class JobhunterApplication {
 
 	public static void main(String[] args) {SpringApplication.run(JobhunterApplication.class, args);
--- src/main/java/vn/tuan/jobhunter/controller/SubscriberController.java
+++ src/main/java/vn/tuan/jobhunter/controller/SubscriberController.java
@@ -2,6 +2,7 @@ package vn.tuan.jobhunter.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import vn.tuan.jobhunter.domain.Subscriber;
@@ -27,5 +28,9 @@ public class SubscriberController {
         Subscriber updated=subscriberService.updateSubscriber(subscriber);
         ApiResponse<Subscriber> apiResponse=new ApiResponse<Subscriber>(HttpStatus.OK,"updated subscrib",updated,null);
         return  ResponseEntity.ok().body(apiResponse);
+    }
+    @Scheduled(fixedDelay = 1000)
+    public void testCron(){
+        System.out.println("cron");
     }
 }
